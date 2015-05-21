16a20d5a9341748de66175d0737e7d24e6747922
MatasT <turskismatas@gmail.com>
2015-05-21 15:34:22 +0200

    small changes in agenda (but commented)

--- WebContent/sortable.js
+++ WebContent/sortable.js
@@ -3,5 +3,11 @@
       connectWith: "ul",
         scroll : false,
         placeholder: "placeholder",
+        /*sort: function() {
+        console.log("Drag fired!");
+        $(".panel-heading").hover(function () {
+            $(this).parent(".panel-default").children('.panel-collapse:not("#collapseMap")').collapse('show');
+        });
+        }*/
     }).disableSelection();
   });
